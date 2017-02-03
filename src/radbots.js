f716f125879517177c72af4b09053aeb231cc118
Anindya Dhruba <anindya@Anindyas-MacBook-Pro.local>
2017-02-03 20:34:29 +0600

    Advertise can be fetched from radbots API

--- src/radbots.js
+++ src/radbots.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 class Radbots {
 		constructor(bp, config) {
 				if (!bp || !config) {
@@ -12,6 +14,37 @@ class Radbots {
 
 		getConfig() {
 				return this.config
+		}
+
+		getAdvertise() {
+				let url = `https://radbots.com/api/ads`;
+
+				return axios.get(url, {
+								params: {
+										agent_key: this.config.agentKey || '',
+										media_type: this.config.mediaType || '',
+										context: this.config.context || '',
+										persona_id: this.config.personaId || '',
+										tags: this.config.tags || '',
+										intent: this.config.intent || '',
+								}
+						})
+						.then(res => this._handleSuccessResponse(res))
+						.catch(err => this._handleErrorResponse(err));
+		}
+
+		_handleSuccessResponse(res) {
+				return res.data;
+		}
+
+		_handleErrorResponse(err) {
+				if (err.response) {
+						throw new Error(`Error from radbots module: ${err.response.data}`);
+				} else if (err.message) {
+						throw new Error(`Error from radbots module: ${err.message}`);
+				} else {
+						throw new Error(`Error from radbots module: ${err}`)
+				}
 		}
 }
 
